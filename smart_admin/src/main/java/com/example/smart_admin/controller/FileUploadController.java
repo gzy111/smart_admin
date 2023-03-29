c5933af0cecb6daee85588e92539957259b9ddb4
zyguan <ziyuan.guan@eisgroup.com>
2023-03-29 18:10:43 +0800

    3.29 文件上传并修改显示图片

--- smart_admin/src/main/java/com/example/smart_admin/controller/FileUploadController.java
+++ smart_admin/src/main/java/com/example/smart_admin/controller/FileUploadController.java
@@ -6,6 +6,7 @@ import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 
+import org.springframework.util.FileSystemUtils;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,10 +27,11 @@ public class FileUploadController {
 
     @Autowired
     DocumentService documentService;
+
     @PostMapping("/UploadImage")
     @ResponseBody
     //将上传的文件放在tomcat目录下面的file文件夹中
-    public String upload(@RequestParam("file")MultipartFile multipartFile, HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public String upload(@RequestParam("file") MultipartFile multipartFile, HttpServletRequest request, HttpServletResponse response) throws IOException {
         //获取到原文件全名
         String originalFilename = multipartFile.getOriginalFilename();
         // request.getServletContext()。getRealPath("")这里不能使用这个，这个是获取servlet的对象，并获取到的一个临时文件的路径，所以这里不能使用这个
@@ -50,80 +52,91 @@ public class FileUploadController {
         //上传文件的全路径
         String url = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/" + format + "/" + originalFilename;
 
-        Document document=new Document();
+        Document document = new Document();
         document.setDocumentUrl("/" + format + "/" + originalFilename);
         document.setDocumentName(originalFilename);
         document.setCreateTime(new Date());
         document.setType(multipartFile.getContentType());
-        System.out.println();
+        System.out.println(document.toString());
 
         documentService.insertSelective(document);
         return url;
 
     }
 
-    @RequestMapping("/download")
-    public void download(HttpServletRequest request, HttpServletResponse response, String name, @RequestParam(defaultValue = "0") int isOnline) throws IOException {
-        try (ServletOutputStream outputStream = response.getOutputStream();) {
-			/*
-			这里要使用ResourceUtils来获取到我们项目的根目录
-			不能使用request.getServletContext().getRealPath("/")，这里获取的是临时文件的根目录（所以不能使用这个）
-			*/
-            String path = ResourceUtils.getURL(ResourceUtils.CLASSPATH_URL_PREFIX).getPath();
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-//            String format = simpleDateFormat.format(new Date());
 
-            String realPath = path + "static/" +  "/" + name;
-            System.out.println(realPath);
-            File file = new File(realPath);
-            //如果下载的文件不存在
-            if (!file.exists()) {
-                response.setContentType("text/html;charset=utf-8");
-                //response.getWriter().write(str);这种写入的就相当于生成了jsp/html，返回html/jsp，所以需要我们进行contentType的设置
-                response.getWriter().write("下载的文件不存在");
-                return;
+
+    @RequestMapping("/downloadFile")
+    public void downloadFiles(@RequestParam("file") String downUrl, HttpServletRequest request, HttpServletResponse response) throws IOException{
+        OutputStream outputStream = null;
+        InputStream inputStream = null;
+        BufferedInputStream bufferedInputStream = null;
+        byte[] bytes = new byte[1024];
+        String path = ResourceUtils.getURL(ResourceUtils.CLASSPATH_URL_PREFIX).getPath();
+        String realPath = path.substring(1) + "static" + downUrl;
+        File file = new File(realPath);
+        String fileName = file.getName();
+        System.out.println("本次下载的文件为" + realPath);
+        // 获取输出流
+        try {
+            // StandardCharsets.ISO_8859_1 *=UTF-8'
+            // response.setHeader("Content-Disposition", "attachment;filename=" +  new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1));
+            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+
+//            response.addHeader("Content-Disposition", "inline;filename=" + new String(fileName.getBytes(), "ISO8859-1"));
+            // 以流的形式返回文件
+            response.setContentType("application/octet-stream;charset=utf-8");
+            inputStream = new FileInputStream(file);
+            bufferedInputStream = new BufferedInputStream(inputStream);
+            outputStream = response.getOutputStream();
+            int i = bufferedInputStream.read(bytes);
+            while (i != -1) {
+                outputStream.write(bytes, 0, i);
+                i = bufferedInputStream.read(bytes);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+                if (bufferedInputStream != null) {
+                    bufferedInputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-            InputStream in = new FileInputStream(realPath);
-            int read;
-
-            //byte[] b = new byte[in.available()];创建一个输入流大小的字节数组，然后把输入流中所有的数据都写入到数组中
-            byte[] b = new byte[1024];
-
-			/*
-			1、Content-Disposition的作用：告知浏览器以何种方式显示响应返回的文件，用浏览器打开还是以附件的形式下载到本地保存
-			2、attachment表示以附件方式下载   inline表示在线打开   "Content-Disposition: inline; filename=文件名.mp3"
-			3、filename表示文件的默认名称，因为网络传输只支持URL编码的相关支付，因此需要将文件名URL编码后进行传输,前端收到后需要反编码才能获取到真正的名称
-			*/
-			/*	注意:文件名字如果是中文会出现乱码：解决办法：
-				1、将name 替换为 new String(filename.getBytes(), "ISO8859-1");
-				2、将name 替换为 URLEncoder.encode(filename, "utf-8");
-			*/
-            if (isOnline == 0) {
-                //在线打开
-                response.addHeader("Content-Disposition", "inline;filename=" + new String(name.getBytes(), "ISO8859-1"));
-                System.out.println(0000);
-            } else {
-                //下载形式，一般跟application/octet-stream一起使用
-                response.addHeader("Content-Disposition", "attachment;filename=" + new String(name.getBytes(), "ISO8859-1"));
-                //如果单纯写这个也可以进行下载功能，表示以二进制流的形式
-                response.setContentType("application/octet-stream");
-            }
-            //文件大小
-            response.addHeader("Content-Length", "" + file.length());
-            while ((read = in.read(b)) > 0) {
-                outputStream.write(b);
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw e;
         }
-    }
 
+
+    }
     @GetMapping("/DocumentList")
     public PageInfo<Document> Documentlist(Document document){
         PageInfo<Document> pageInfo = documentService.selectByPrimaryKey(document);
         return pageInfo;
+    }
+
+
+    /**
+     * 删除指定文件夹或文件
+     * @param path 文件夹或文件路径
+     * @return Boolean 删除成功返回true，删除失败返回false。
+     */
+    @GetMapping("/delete")
+    public Boolean uploadCustomPathFile(@RequestParam("file")String file,@RequestParam("id")int id) throws FileNotFoundException {
+        String path = ResourceUtils.getURL(ResourceUtils.CLASSPATH_URL_PREFIX).getPath();
+        String realPath = path.substring(1) + "static" + file;
+        boolean re=FileSystemUtils.deleteRecursively(new File(realPath));
+        if (re){
+            documentService.deleteByPrimaryKey(id);
+        }
+
+        return re;
     }
 
 }
